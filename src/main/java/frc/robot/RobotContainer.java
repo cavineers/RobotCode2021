9a0fd12f8c5a3653db55d1900b8ff89dd3758ce2
topmastered <bryce@brycecary.dev>
2021-01-14 19:24:25 -0500

    Initial Project Push

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class RobotContainer {
+	public RobotContainer() {
+		configureButtonBindings();
+	}
+
+	private void configureButtonBindings() {}
+
+	// public Command getAutonomousCommand() {
+	// 	return m_autoCommand;
+	// }
+}
