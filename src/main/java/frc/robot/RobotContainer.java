dd8b8f691dbd80e92fa2a54cde22fe55f6d82f00
topmastered <bryce@brycecary.dev>
2021-02-16 19:16:43 -0500

    Updated container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,33 +4,20 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 public class RobotContainer {
 	// Controller
-	public static Joystick joy;
-    public static JoystickButton a_button;
-    public static JoystickButton b_button;
-    public static JoystickButton x_button;
-    public static JoystickButton y_button;
-    public static JoystickButton l_bump;
-    public static JoystickButton r_bump;
-    public static JoystickButton left_menu;
-    public static JoystickButton right_menu;
-    public static JoystickButton left_stick;
-    public static JoystickButton right_stick;
+	public Joystick joy = new Joystick(0);
+    public JoystickButton a_button = new JoystickButton(joy, 1);
+    public JoystickButton b_button = new JoystickButton(joy, 2);
+    public JoystickButton x_button = new JoystickButton(joy, 3);
+    public JoystickButton y_button = new JoystickButton(joy, 4);
+    public JoystickButton l_bump = new JoystickButton(joy, 5);
+    public JoystickButton r_bump = new JoystickButton(joy, 6);
+    public JoystickButton left_menu = new JoystickButton(joy, 7);
+    public JoystickButton right_menu = new JoystickButton(joy, 8);
+    public JoystickButton left_stick = new JoystickButton(joy, 9);
+    public JoystickButton right_stick = new JoystickButton(joy, 10);
 
 	public RobotContainer() {
 		Robot.logger.addInfo("RobotContainer", "Created RobotContainer");
-
-		// Setup Controller
-		joy = new Joystick(0);
-		a_button = new JoystickButton(joy, 1);
-		b_button = new JoystickButton(joy, 2);
-		x_button = new JoystickButton(joy, 3);
-		y_button = new JoystickButton(joy, 4);
-		l_bump = new JoystickButton(joy, 5);
-		r_bump = new JoystickButton(joy, 6);
-		left_menu = new JoystickButton(joy, 7);
-		right_menu = new JoystickButton(joy, 8);
-		left_stick = new JoystickButton(joy, 9);
-		right_stick = new JoystickButton(joy, 10);
 
 		// Controller Bindings
 		mapButtonBindings();
