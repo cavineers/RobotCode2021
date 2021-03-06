fac0caadaed4565d7b3625090868c8cc408397bc
topmastered <bryce@brycecary.dev>
2021-03-05 22:25:48 -0500

    fix(Paths): update to match Path

--- src/main/java/frc/robot/paths/ExamplePath.java
+++ src/main/java/frc/robot/paths/ExamplePath.java
@@ -12,18 +12,18 @@ public class ExamplePath extends Path {
     public ExamplePath() {
         super();
 
-        this.addPlot(50.0, 0.0, -180.0, 1, 1);
-        this.addPlot(95.0, 65.0, 0.0, 1, 1);
-        this.addPlot(150.0, 80.0, 0.0, 1, 1);
-        this.addPlot(220.0, 50.0, 0.0, 1, 1);
-        this.addPlot(275.0, -5.0, 60.0, 1, 1);
-        this.addPlot(315.0, 31.0, -90.0, 1, 1);
-        this.addPlot(275.0, 70.0, -180.0, 1, 1);
-        this.addPlot(220.0, 0.0, 180.0, 1, 1);
-        this.addPlot(160.0, -5.0, 180.0, 1, 1);
-        this.addPlot(75.0, 0.0, 180.0, 1, 1);
-        this.addPlot(52.0, 61.0, 90.0, 1, 1);
-        this.addPlot(0.0, 65.0, 0.0, 1, 1);
+        this.add(50.0, 0.0, -180.0, 1, 1);
+        this.add(95.0, 65.0, 0.0, 1, 1);
+        this.add(150.0, 80.0, 0.0, 1, 1);
+        this.add(220.0, 50.0, 0.0, 1, 1);
+        this.add(275.0, -5.0, 60.0, 1, 1);
+        this.add(315.0, 31.0, -90.0, 1, 1);
+        this.add(275.0, 70.0, -180.0, 1, 1);
+        this.add(220.0, 0.0, 180.0, 1, 1);
+        this.add(160.0, -5.0, 180.0, 1, 1);
+        this.add(75.0, 0.0, 180.0, 1, 1);
+        this.add(52.0, 61.0, 90.0, 1, 1);
+        this.add(0.0, 65.0, 0.0, 1, 1);
 
         this.finish();
     }
