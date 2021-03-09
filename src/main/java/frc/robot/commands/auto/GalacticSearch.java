1535dd76b618b99cf46aac4949cb0f17cc33b0ec
Bryce Cary <bryce@brycecary.dev>
2021-03-09 10:38:23 -0500

    fix(autonomous): update galactic search

--- src/main/java/frc/robot/commands/auto/GalacticSearch.java
+++ src/main/java/frc/robot/commands/auto/GalacticSearch.java
@@ -7,7 +7,6 @@ import frc.lib.Target;
 import frc.robot.Constants;
 import frc.robot.Robot;
 import frc.robot.commands.TimedToggleIntake;
-import frc.robot.subsystems.SwerveDrive.SwerveDriveState;
 
 /**
  * Galactic Search autonomous command.
@@ -15,7 +14,7 @@ import frc.robot.subsystems.SwerveDrive.SwerveDriveState;
  */
 public class GalacticSearch extends CommandBase {
     // ToggleIntake Command
-    private Command m_toggleIntake = new TimedToggleIntake(5); // TODO update will tuned value
+    private Command m_toggleIntake = new TimedToggleIntake(5); // TODO Should be replaced with a command that uses feedback
 
     // PIDs
     private PIDController m_td = new PIDController(Constants.ObjVision.kDistancePID_P, 
@@ -24,33 +23,39 @@ public class GalacticSearch extends CommandBase {
             Constants.ObjVision.kXPID_I, Constants.ObjVision.kXPID_D);
 
     public GalacticSearch() {
-        addRequirements(Robot.swerveDrive);
+        this.addRequirements(Robot.swerveDrive);
     }
 
     @Override
     public void initialize() {
         Robot.logger.addInfo("GalacticSearch", "Galactic Search Autonomous Command Activated.");
+
         m_td.setTolerance(Constants.ObjVision.kDistancePID_Tolerance);
         m_a.setTolerance(Constants.ObjVision.kXPID_Tolerance);
+
+        m_td.setSetpoint(0);
+        m_a.setSetpoint(0);
     }
 
     @Override
     public void execute() {
-        Target m_closestPowerCell = Robot.vision.getPowerCellTarget();
+        Target closestPowerCell = Robot.vision.getPowerCellTarget();
 
         // Calculations
-        double td = m_closestPowerCell.getDistance(); // distance y (b) value in law of sines
-        double tx = m_closestPowerCell.getTx(); // A angle in law of sines
+        double td = closestPowerCell.getDistance(); // distance y (b) value in law of sines
+        double tx = closestPowerCell.getTx(); // A angle in law of sines
         double a = Math.sqrt(Math.pow((td / Math.sin(90 - tx)), 2) + Math.pow(td, 2)); // distance x (a) in law of sines
 
         this.m_td.calculate(td);
         this.m_a.calculate(a);
 
-        if(td >= 2 && !m_toggleIntake.isScheduled()) {
-            // Drive the robot based on the coordinates of power cell
-            Robot.swerveDrive.swerve(this.m_td.getSetpoint(), this.m_a.getSetpoint(), 0, false);
-        } else if(!m_toggleIntake.isScheduled()) {
+        if (!this.m_toggleIntake.isScheduled() && this.m_td.atSetpoint() && this.m_a.atSetpoint()) {
             this.m_toggleIntake.schedule();
+        } else if (this.m_toggleIntake.isFinished()) {
+            this.m_toggleIntake = new TimedToggleIntake(5); // TODO Should be replaced with a command that uses feedback
+        } else if (!this.m_toggleIntake.isScheduled()) {
+            // Drive the robot based on the coordinates of power cell
+            Robot.swerveDrive.swerve(this.m_td.calculate(td), this.m_a.calculate(a), 0, false);
         }
     }
 
@@ -62,6 +67,7 @@ public class GalacticSearch extends CommandBase {
 
     @Override
     public boolean isFinished() {
-        return Robot.swerveDrive.getState() == SwerveDriveState.SWERVE;
+        return false;
+        // return Robot.transportation.getBallCount() == 3; // TODO: Implement this missing method
     }
 }
