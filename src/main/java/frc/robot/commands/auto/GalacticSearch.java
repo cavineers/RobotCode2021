61308ad48adbab455a25695bab1bbeda4bd168a9
Ryan Bode <rbcrusher88@gmail.com>
2021-03-14 19:26:40 -0400

    fix: galactic search running off constantly running intake

--- src/main/java/frc/robot/commands/auto/GalacticSearch.java
+++ src/main/java/frc/robot/commands/auto/GalacticSearch.java
@@ -1,21 +1,17 @@
 package frc.robot.commands.auto;
 
 import edu.wpi.first.wpilibj.controller.PIDController;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.lib.Target;
 import frc.robot.Constants;
 import frc.robot.Robot;
-import frc.robot.commands.TimedToggleIntake;
+import frc.robot.commands.ToggleIntake;
 
 /**
  * Galactic Search autonomous command.
  * Note: Tolerance for Vision calcs is +- 2 inches
  */
 public class GalacticSearch extends CommandBase {
-    // ToggleIntake Command
-    private Command m_toggleIntake = new TimedToggleIntake(5); // TODO Should be replaced with a command that uses feedback
-
     // Finished
     private boolean m_finished = false;
 
@@ -33,6 +29,9 @@ public class GalacticSearch extends CommandBase {
     public void initialize() {
         Robot.logger.addInfo("GalacticSearch", "Galactic Search Autonomous Command Activated.");
 
+        //Turns Intake Subsystem On.
+        new ToggleIntake();
+
         m_td.setTolerance(Constants.ObjVision.kDistancePID_Tolerance);
         m_a.setTolerance(Constants.ObjVision.kXPID_Tolerance);
 
@@ -42,26 +41,18 @@ public class GalacticSearch extends CommandBase {
 
     @Override
     public void execute() {
-        if (Robot.transportation.getBallCount() == 3) {
-            Target closestPowerCell = Robot.vision.getPowerCellTarget();
+        Target closestPowerCell = Robot.vision.getPowerCellTarget();
 
-            // Calculations
-            double td = closestPowerCell.getDistance(); // distance y (b) value in law of sines
-            double tx = closestPowerCell.getTx(); // A angle in law of sines
-            double a = Math.sqrt(Math.pow((td / Math.sin(90 - tx)), 2) + Math.pow(td, 2)); // distance x (a) in law of sines
+        // Calculations
+        double td = closestPowerCell.getDistance(); // distance y (b) value in law of sines
+        double tx = closestPowerCell.getTx(); // A angle in law of sines
+        double a = Math.sqrt(Math.pow((td / Math.sin(90 - tx)), 2) + Math.pow(td, 2)); // distance x (a) in law of sines
 
-            this.m_td.calculate(td);
-            this.m_a.calculate(a);
+        this.m_td.calculate(td);
+        this.m_a.calculate(a);
 
-            if (!this.m_toggleIntake.isScheduled() && this.m_td.atSetpoint() && this.m_a.atSetpoint()) {
-                this.m_toggleIntake.schedule();
-            } else if (this.m_toggleIntake.isFinished()) {
-                this.m_toggleIntake = new TimedToggleIntake(5); // TODO Should be replaced with a command that uses feedback
-            } else if (!this.m_toggleIntake.isScheduled()) {
-                // Drive the robot based on the coordinates of power cell
-                Robot.swerveDrive.swerve(this.m_td.calculate(td), this.m_a.calculate(a), 0, false);
-            }
-        }
+        // Drive the robot based on the coordinates of power cell
+        Robot.swerveDrive.swerve(this.m_td.calculate(td), this.m_a.calculate(a), 0, false);
     }
 
     @Override
