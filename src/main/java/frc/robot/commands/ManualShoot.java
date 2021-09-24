df5dbe8503d980b3cd1a78b72cfd25b48d11f8c4
Ryan Bode <rbcrusher88@gmail.com>
2021-09-24 09:00:39 -0400

    feat: manual shooting sequence

--- src/main/java/frc/robot/commands/ManualShoot.java
+++ src/main/java/frc/robot/commands/ManualShoot.java
@@ -1,44 +1,50 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.Timer;
+// import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
-import frc.robot.subsystems.Transportation.TransportMotorState;
+import frc.robot.subsystems.Shooter.ShooterMode;
+// import frc.robot.subsystems.Transportation.TransportMotorState;
 
 /**
  * Turns on shooting systems and conveyors.
  */
 public class ManualShoot extends CommandBase {
-    private double m_timestamp;
+    // private double m_timestamp;
 
     public ManualShoot() {}
 
     @Override
     public void initialize() {
-        Robot.shooter.enable();
-        Robot.shooter.setSpeed(4500); // Change to update speed. (1000 - 5000 safe bounds)
+        if (Robot.shooter.getCurrentMode() == ShooterMode.DISABLED) {
+            Robot.shooter.enable();
+            Robot.shooter.setSpeed(4500); // Change to update speed. (1000 - 5000 safe bounds)
+        } else if (Robot.shooter.getCurrentMode() == ShooterMode.ENABLED) {
+            Robot.shooter.disable();
+        }
 
-        this.m_timestamp = Timer.getFPGATimestamp();
+        // this.m_timestamp = Timer.getFPGATimestamp();
     }
 
     @Override
     public void execute() {
-        if (Timer.getFPGATimestamp() - this.m_timestamp >= 2.0) {
-            Robot.transportation.setConveyorMotorState(TransportMotorState.ON);
-            Robot.transportation.setFeederMotorState(TransportMotorState.ON);
-        }
+        // if (Timer.getFPGATimestamp() - this.m_timestamp >= 2.0) {
+        //     Robot.transportation.setConveyorMotorState(TransportMotorState.ON);
+        //     Robot.transportation.setFeederMotorState(TransportMotorState.ON);
+        // }
     }
 
     @Override
     public void end(boolean interrupted) {
-        Robot.shooter.disable();
-        Robot.transportation.setConveyorMotorState(TransportMotorState.OFF);
-        Robot.transportation.setFeederMotorState(TransportMotorState.OFF);
+        // Robot.shooter.disable();
+        // Robot.transportation.setConveyorMotorState(TransportMotorState.OFF);
+        // Robot.transportation.setFeederMotorState(TransportMotorState.OFF);
     }
 
     @Override
     public boolean isFinished() {
+        return true;
         // Turns off after 10 seconds.
-        return Timer.getFPGATimestamp() - this.m_timestamp >= 10.0;
+        // return Timer.getFPGATimestamp() - this.m_timestamp >= 10.0;
     }
 }
